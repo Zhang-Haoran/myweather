a051df9d00f27f3ce2bbcadb8b8443b1f0f47eec
haoranzhang <haoranzhang001@gmail.com>
2021-05-25 01:13:19 +1000

    short circuit calculation短路计算

--- src/pages/components/home/Home.js
+++ src/pages/components/home/Home.js
@@ -7,10 +7,10 @@ import {getCurrentWeatherFromAPI, getForecastWeatherFromAPI} from "../../../serv
 import ForecastWeather from "./components/ForecastWeather";
 
 //主页背景图片
-const HomeBackground = styled.div`
+const Background = styled.div`
   background: url(${props => props.img});
   height: 100vh;
-  background-size:100% 100%;
+  background-size: 100% 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -20,7 +20,7 @@ const Home = () => {
     //搜索栏数值
     const [searchBarValue, setSearchBarValue] = useState("");
     //当前天气，从后端获取
-    const [currentWeather,setCurrentWeather] = useState(undefined);
+    const [currentWeather, setCurrentWeather] = useState(undefined);
     //天气预报，从后端获取
     const [forecastWeather, setForecastWeather] = useState(undefined);
     //当输入框发生变化时
@@ -29,40 +29,33 @@ const Home = () => {
         setSearchBarValue(event.target.value)
     }
     //提交搜索表单
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         //阻止页面跳转
         event.preventDefault();
         //从后端获取天气数据，异步
-        getCurrentWeatherFromAPI(searchBarValue).then((result)=>{
+        getCurrentWeatherFromAPI(searchBarValue).then((result) => {
             //改变当天天气的state
             setCurrentWeather(result);
         });
         //从后端获取天气预报，异步
-        getForecastWeatherFromAPI(searchBarValue).then((result)=>{
+        getForecastWeatherFromAPI(searchBarValue).then((result) => {
             //改变天气预报的state
             setForecastWeather(result);
         })
     }
 
-    //条件渲染，当当前天气和天气预报的state不为undefined，说明从后端获取了数据
-    if (currentWeather !== undefined && forecastWeather !== undefined){
-        return(
-            // 传入背景图片，需要import
-            <HomeBackground img={img}>
-                {/*把onchange作为props传给子组件*/}
-                <SearchBar value = {searchBarValue} onChange = {handleSearchBarChange} submit = {handleSubmit} isResultFetched={true}/>
-                {/*把当前天气的state传入 下一个组件*/}
-                <CurrentWeather currentWeather = {currentWeather}/>
-                {/*把天气预报state传入 下一个组件*/}
-                <ForecastWeather forecastWeather={forecastWeather}/>
-            </HomeBackground>
-        )
-    }
-    return(
-        <HomeBackground img={img}>
-            {/*把onchange作为props传给子组件*/}
-            <SearchBar value = {searchBarValue} onChange = {handleSearchBarChange} submit = {handleSubmit} isResultFetched ={false}/>
-        </HomeBackground>
+    //短路计算，当当前天气和天气预报的state不为undefined，显示当前天气和天气预报
+    return (
+        <Background img={img}>
+            {(currentWeather && forecastWeather) &&
+            <SearchBar value={searchBarValue} onChange={handleSearchBarChange} submit={handleSubmit}
+                       isResultFetched={true}/>}
+            {(currentWeather && forecastWeather) && <CurrentWeather currentWeather={currentWeather}/>}
+            {(currentWeather && forecastWeather) && <ForecastWeather forecastWeather={forecastWeather}/>}
+            {!(currentWeather && forecastWeather) &&
+            <SearchBar value={searchBarValue} onChange={handleSearchBarChange} submit={handleSubmit}
+                       isResultFetched={false}/>}
+        </Background>
     )
 }
 export default Home;
