77a9ac7a9c0a7e5f6b8b4fe36e1f0d1b3a9c60a9
haoranzhang <haoranzhang001@gmail.com>
2021-05-25 00:31:46 +1000

    styled component reformat for reuse

--- src/pages/components/home/components/CurrentWeather.js
+++ src/pages/components/home/components/CurrentWeather.js
@@ -1,6 +1,44 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import ReactAnimatedWeather from 'react-animated-weather';
 import {findWeatherIcon} from "../../../../utils/findWeatherIcon";
+
+const Flex = styled.div`
+  display: flex;
+  ${(props) => props.type === "Space-around" && css`
+    justify-content: space-around;
+  `}
+  ${(props) => props.type === "Vertical" && css`
+    flex-direction: column;
+  `}
+`;
+
+const Description = styled.span`
+  margin-left: 3rem;
+  font-size: 2rem;
+  ${(props) => props.type === "Temperature" && css`
+    font-size: 5rem;
+  `}
+`;
+
+const Card = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  border-radius: 1rem;
+  font-size: 1.5rem;
+  padding: 1rem;
+
+  &:hover {
+    box-shadow: 0 0.2rem 0.5rem 0 rgba(0, 0, 0, 0.5);
+  }
+
+  ${(props) => props.type === "Secondary" && css`
+    flex-direction: column;
+    justify-content: space-around;
+    background-color: rgba(255, 255, 255, 0.2);
+    font-size: 1.5rem;
+    backdrop-filter: blur(10px);
+  `}
+`;
 
 const Title = styled.h1`
   margin-top: 5rem;
@@ -15,71 +53,27 @@ const Subtitle = styled.h2`
   margin-top: 0;
 `;
 
-const InfoSection = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-
-const PrimaryInfo = styled.div`
-  margin-top: 3rem;
-  display: flex;
-  border-radius: 1rem;
-  padding: 1rem;
-
-  &:hover {
-    box-shadow: 0 0.2rem 0.5rem 0 rgba(0, 0, 0, 0.5);
-  }
-`;
-const FlexVertical = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const WeatherDescription = styled.span`
-  margin-left: 3rem;
-  font-size: 2rem;
-`;
-const WeatherTemperature = styled.span`
-  margin-left: 3rem;
-  font-size: 5rem;
-`;
-
-const SecondaryInfo = styled.span`
-  margin-top: 3rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 1rem;
-  font-size: 1.5rem;
-  padding: 1rem;
-
-  &:hover {
-    box-shadow: 0 0.2rem 0.5rem 0 rgba(0, 0, 0, 0.5);
-  }
-`;
-
 //当前天气组件
 const CurrentWeather = ({currentWeather}) => {
     return (
         <>
             <Title>{currentWeather.city}</Title>
             <Subtitle>{currentWeather.date}</Subtitle>
-            <InfoSection>
-                <PrimaryInfo>
+            <Flex type="Space-around">
+                <Card type="Primary">
                     <ReactAnimatedWeather
                         icon={findWeatherIcon(currentWeather.weather, new Date().toTimeString().split(" ")[0])}
                         size={128}/>
-                    <FlexVertical>
-                        <WeatherTemperature>{currentWeather.currentTemperature}&#176;</WeatherTemperature>
-                        <WeatherDescription>{currentWeather.weatherDescription}</WeatherDescription>
-                    </FlexVertical>
-                </PrimaryInfo>
-                <SecondaryInfo>
+                    <Flex type="Vertical">
+                        <Description type="Temperature">{currentWeather.currentTemperature}&#176;</Description>
+                        <Description type="Description">{currentWeather.weatherDescription}</Description>
+                    </Flex>
+                </Card>
+                <Card type="Secondary">
                     <div>Highest: {currentWeather.highestTemperature}&#176; Lowest: {currentWeather.lowestTemperature}&#176;</div>
                     <div>Humidity: {currentWeather.humidity}% Wind: {currentWeather.windSpeed}mph</div>
-                </SecondaryInfo>
-            </InfoSection>
+                </Card>
+            </Flex>
         </>
     )
 }
