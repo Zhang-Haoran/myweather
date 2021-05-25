d1f2aec03e83e4f2d160006eb0e8f253d9d07b02
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-05-25 11:04:08 +1000

    use object to replace switch

--- src/utils/findWeatherIcon.js
+++ src/utils/findWeatherIcon.js
@@ -1,30 +1,15 @@
 //根据天气预报的气象信息 找到对应的天气图标
 export const findWeatherIcon = (eachWeather,eachTime) => {
     const hours = eachTime.substr(0,2);
-    switch (eachWeather){
-        case "Clear":
-            return hours >= 18 || hours <= 5 ? "CLEAR_NIGHT": "CLEAR_DAY";
-        case "Clouds":
-            return hours >= 18 || hours <= 5 ? "PARTLY_CLOUDY_NIGHT": "PARTLY_CLOUDY_DAY";
-        case "Drizzle":
-            return "RAIN";
-        case "Rain" || "Thunderstorm":
-            return "SLEET";
-        case "Snow":
-            return "SNOW";
-        case "Fog":
-            return "FOG";
-        default:
-            return "CLOUDY";
+    const weatherIcon = {
+        "Clear": hours >= 18 || hours <= 5 ? "CLEAR_NIGHT": "CLEAR_DAY",
+        "Clouds": hours >= 18 || hours <= 5 ? "PARTLY_CLOUDY_NIGHT": "PARTLY_CLOUDY_DAY",
+        "Drizzle": "RAIN",
+        "Rain": "SLEET",
+        "Thunderstorm": "SLEET",
+        "Snow": "SNOW",
+        "Fog": "FOG",
+        "Default": "Cloudy"
     }
-    // 维尼老师建议写法
-    // return{
-    //     "Clear": hours >= 18 || hours <= 5 ? "CLEAR_NIGHT": "CLEAR_DAY",
-    //     "Clouds": hours >= 18 || hours <= 5 ? "PARTLY_CLOUDY_NIGHT": "PARTLY_CLOUDY_DAY",
-    //     "Drizzle": "RAIN",
-    //     "Rain": "SLEET",
-    //     "Thunderstorm": "SLEET",
-    //     "Snow": "SNOW",
-    //     "Fog": "FOG",
-    // }[eachWeather]
+    return weatherIcon[eachWeather] || weatherIcon["Default"];
 }
